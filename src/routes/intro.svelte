41e6a8f75e0280edcac7f83a710267341e7fa3b5
Freddy Linn <frederick.linn@colorado.edu>
2024-08-31 17:33:38 -0600

    add prettier formatter

--- src/routes/intro.svelte
+++ src/routes/intro.svelte
@@ -1,10 +1,13 @@
 <div class="bg-[url(/landscape9.jpg)] w-screen bg-cover bg-no-repeat flex flex-wrap justify-center items-center p-4 gap-4">
-    <img class="rounded-full w-64 h-64 object-cover shadow-md" src="/portrait.jpg" alt="test">
+    <img class="rounded-full w-64 h-64 object-cover shadow-md" src="/portrait.jpg" alt="test" />
     <div class="w-5/ md:w-1/2 bg-slate-800/70 backdrop-blur-sm rounded-md text-center font-mono text-white p-4">
         <h3 class="font-bold font-mono text-3xl text-white drop-shadow-md">Freddy Linn</h3>
         <h4 class="font-bold font-mono text-md drop-shadow-md mb-4">Cybersecurity & Software Development</h4>
-        <p class="font-medium text-lg md:text-left md:indent-4">Hello! I'm currently in my senior year at CU Boulder majoring in Computer Science. I believe in writing secure, accessible, responsive, and readable code.
-            My experience in both security and software gives me a more holistic understanding of each field, and my extensive work in development teams has taught me how to collaborate with both team members and stakeholders.
+        <p class="font-medium text-lg md:text-left md:indent-4">
+            Hello! I'm currently in my senior year at CU Boulder majoring in Computer Science. I believe in writing secure,
+            accessible, responsive, and readable code. My experience in both security and software gives me a more holistic
+            understanding of each field, and my extensive work in development teams has taught me how to collaborate with both
+            team members and stakeholders.
         </p>
     </div>
 </div>
--- src/routes/skills.svelte
+++ src/routes/skills.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
-
     import { fade } from "svelte/transition";
 
     type TabContentType = {
         [key: string]: string[];
-    }
+    };
 
     const tabTitles = ["Languages", "Frameworks", "Security", "Databases", "Cloud", "Dev Tools"];
     let selectedTab: keyof TabContentType = tabTitles[0];
 
     // Each tab contains an object made up of technology mapped to its icon name
     const tabContent: TabContentType = {
-        "Languages": ["Javascript", "Typescript", "SQL", "Python", "HTML", "CSS", "C++", "C", "Java", "Scala"],
-        "Frameworks": ["React"]
-    }
-
+        Languages: ["Javascript", "Typescript", "SQL", "Python", "HTML", "CSS", "C++", "C", "Java", "Scala"],
+        Frameworks: ["React"],
+    };
 </script>
 
 <section class="bg-zinc-800 text-white">
     <h2 class="text-3xl font-semibold font-sans text-center pt-8 pb-6">Technical Skills</h2>
     <div class="flex justify-center gap-2 flex-wrap px-8">
         {#each tabTitles as title}
-            <button on:click={() => selectedTab = title} class="border rounded-full px-5 py-2 font-medium text-lg {selectedTab === title ? 'bg-white text-black' : ''}">
+            <button
+                on:click={() => (selectedTab = title)}
+                class="border rounded-full px-5 py-2 font-medium text-lg {selectedTab === title ? 'bg-white text-black' : ''}"
+            >
                 {title}
             </button>
         {/each}
     </div>
     <div class="flex justify-center items-center gap-16 flex-wrap p-8">
         {#each tabContent[selectedTab] as tool}
-        <div class="flex flex-col items-center justify-center">
-            <svg class="h-24 w-24 mb-2"><use href={`/icons.svg#${tool.toLowerCase()}`}></use></svg>
-            <h3 class="font-medium font-sans text-xl">{tool}</h3>
-        </div>
+            <div class="flex flex-col items-center justify-center">
+                <svg class="h-24 w-24 mb-2"><use href={`/icons.svg#${tool.toLowerCase()}`}></use></svg>
+                <h3 class="font-medium font-sans text-xl">{tool}</h3>
+            </div>
         {/each}
     </div>
-
 </section>
