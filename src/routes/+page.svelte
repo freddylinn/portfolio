5b6e0bee0c6519b37b1f45538bd14a211d9ecb7b
Freddy Linn <frederick.linn@colorado.edu>
2024-09-02 02:43:11 -0600

    add scaffolding for projects section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Intro from "./intro.svelte";
     import Skills from "./skills.svelte";
+    import Projects from "./projects.svelte";
 </script>
 
 <Intro />
 <Skills />
+<Projects />
---
+++ src/routes/projects.svelte
@@ -0,0 +1,20 @@
+<script>
+    import projectList from "$lib/projectList.json";
+</script>
+
+<div
+    class="bg-[url(/landscape8.jpg)] w-screen bg-cover bg-no-repeat flex flex-wrap justify-center items-center px-4 py-12 gap-4 text-white"
+>
+    <h2 class="font-semibold font-sans text-3xl bg-slate-800/70 rounded-xl px-8 py-2">Projects</h2>
+    {#each projectList as project}
+        <div class="bg-slate-800/70 backdrop-blur-sm rounded-md">
+            <h3>{project.title}</h3>
+            <div class="flex gap-3">
+                {#each project.skills as skill}
+                    <svg class="h-8 w-8 mb-2"><use href={`/icons.svg#${skill.toLowerCase()}`}></use></svg>
+                {/each}
+            </div>
+            <p>{project.summary}</p>
+        </div>
+    {/each}
+</div>
