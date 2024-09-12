63a75c857e3532a74a1e0719579af25ab8017031
Freddy <frederick.linn@colorado.edu>
2024-09-12 13:28:12 -0600

    add contact section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
     import Intro from "./intro.svelte";
     import Skills from "./skills.svelte";
     import Projects from "./projects.svelte";
+    import Contact from "./contact.svelte";
 </script>
 
 <Intro />
 <Skills />
 <Projects />
+<Contact />
---
+++ src/routes/contact.svelte
@@ -0,0 +1,23 @@
+<section class="bg-zinc-800 text-white">
+    <h2 class="text-3xl font-semibold font-sans text-center pt-8 pb-6">Contact Me</h2>
+    <div class="flex flex-col items-center gap-2">
+        <div class="flex items-center">
+            <a
+                href="mailto:frederick.linn@colorado.edu"
+                class="px-5 py-2 mb-4 flex items-center text-lg bg-neutral-100 hover:bg-neutral-200 rounded-full"
+            >
+                <svg class="h-8 w-8 mr-3" aria-hidden="true"><use href="/icons.svg#mail"></use></svg>
+                <span class="text-black mr-1">frederick.linn@colorado.edu</span>
+            </a>
+        </div>
+        <div class="flex items-center">
+            <a
+                href="https://www.linkedin.com/in/frederick-linn"
+                class="px-5 py-2 mb-10 flex items-center text-lg bg-neutral-100 hover:bg-neutral-200 rounded-full"
+            >
+                <svg class="h-8 w-8 mr-3" aria-hidden="true"><use href="/icons.svg#linkedin"></use></svg>
+                <span class="text-black mr-1">frederick-linn</span>
+            </a>
+        </div>
+    </div>
+</section>
