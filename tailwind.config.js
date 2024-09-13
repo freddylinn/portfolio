8c0a3c2736acf73c7e3fcf90ebc34ac0f294e23f
Freddy <frederick.linn@colorado.edu>
2024-09-12 22:36:37 -0600

    add custom fonts and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    theme: {
+        fontFamily: {
+            'sans': ["inter", "ui-sans-serif", "system-ui", 'sans-serif'],
+            'mono': ["fira", "ui-monospace", "SFMono-Regular", 'monospace'],
+        },
+        extend: {
+          spacing: {
+            '112': '28rem',
+            '128': '32rem'
+          }
+        },
+    },
+    plugins: [],
+};
