c9fbe4dc24f25a998e4e944404b9c983b2a4827c
Freddy <frederick.linn@colorado.edu>
2024-09-14 02:27:01 -0600

    add more text to intro and reduce vertical padding accordingly

--- src/routes/intro.svelte
+++ src/routes/intro.svelte
@@ -1,5 +1,5 @@
 <div
-    class="bg-[url(/landscape9.jpg)] w-screen bg-cover bg-no-repeat flex flex-wrap justify-center items-center px-4 py-12 gap-4 lg:gap-8 lg:py-36"
+    class="bg-[url(/landscape9.jpg)] w-screen bg-cover bg-no-repeat flex flex-wrap justify-center items-center px-4 py-12 gap-4 lg:gap-8 lg:py-20"
 >
     <img
         class="rounded-full w-64 h-64 md:w-96 md:h-96 object-cover shadow-md"
@@ -9,11 +9,18 @@
     <div class="w-5/ md:w-1/2 bg-slate-800/70 backdrop-blur-sm rounded-md text-center font-mono text-white p-4">
         <h3 class="font-semibold mb-1 text-3xl md:text-5xl drop-shadow-md">Freddy Linn</h3>
         <h4 class="font-medium text-lg drop-shadow-md mb-4">Cybersecurity & Software Development</h4>
-        <p class="font-regular text-lg md:text-xl md:text-left md:indent-4">
+        <p class="font-regular text-lg leading-relaxed lg:leading-relaxed md:text-xl md:indent-8">
             Hello! I'm currently in my senior year at CU Boulder majoring in Computer Science. I believe in writing secure,
             accessible, responsive, and readable code. My experience in both security and software gives me a more holistic
             understanding of each field, and my extensive work in development teams has taught me how to collaborate with both
-            team members and stakeholders.
+            team members and stakeholders. <a
+                class="text-blue-300 underline"
+                target="_blank"
+                href="https://www.blueprintboulder.org">Blueprint Boulder</a
+            >
+            has been an incredible part of my time at CU and my main extracurricular. I've worked in various roles for this club including
+            co-president, project lead, webmaster, outreach, and a project team member. This has improved my consulting and communication
+            skills as we work directly with nonprofits throughout the year.
         </p>
     </div>
 </div>
