710226258476267e78e378daa2e64ab84327e0b7
Freddy <frederick.linn@colorado.edu>
2024-09-19 00:43:41 -0600

    add vercel web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import "../app.css";
     import { inject } from "@vercel/analytics";
+    import { dev } from "$app/environment";
+    inject({ mode: dev ? "development" : "production" });
 </script>
 
 <slot />
